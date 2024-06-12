81cfbf8bc6542eed2fe287010425290884c9ac62
floatingheaven <sasalkinesketit@gmail.com>
2024-06-12 23:55:03 +0300

    Major changes in index.html, backend for my FriedsRecomendation function. Now its return choosen radio info.

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form.get('rating', False)
+        comments = request.form['comments']
+        friendrec = request.form.get('ratingf', False)
+        print(customer, dealer,  comments, friendrec, rating)
+        return render_template('success.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
