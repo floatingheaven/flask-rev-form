c686919d7d14e82ec0afc510c8ba85467625a262
floatingheaven <sasalkinesketit@gmail.com>
2024-06-14 16:40:43 +0300

    Major changes in index.html, backend for my FriedsRecomendation function. Now its return choosen radio info.

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask,render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+ENV = 'dev'
+
+if ENV == 'def':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Piwdiepay123localhost/lada'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Piwdiepay123localhost/lada'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Feedvack()
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -15,8 +30,10 @@ def submit():
         rating = request.form.get('rating', False)
         comments = request.form['comments']
         friendrec = request.form.get('ratingf', False)
-        print(customer, dealer,  comments, friendrec, rating)
+        # print(customer, dealer,  comments, friendrec, rating)
+        if customer =='' or dealer == '':
+            return render_template('index.html', message='Пожалуйста, заполните всю форму')
         return render_template('success.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
